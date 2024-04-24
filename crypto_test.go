c352e14ecbd4492c597f234ee62862ef59263874
Tomás Sarmiento <t.sarmiento01@ufromail.cl>
2024-04-24 14:04:31 -0400

    feat: add encryption decryption support

---
+++ crypto_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCopyEncryptDecrypt(t *testing.T) {
+	payload := "Foo not Bar"
+	src := bytes.NewReader([]byte(payload))
+	dst := new(bytes.Buffer)
+	key := newEncryptionKey()
+	_, err := copyEncrypt(key, src, dst)
+	if err != nil {
+		t.Error(err)
+	}
+
+	out := new(bytes.Buffer)
+	if _, err := copyDecrypt(key, dst, out); err != nil {
+		t.Error(err)
+	}
+
+	if out.String() != payload {
+		t.Errorf("decryption failed")
+	}
+
+}
